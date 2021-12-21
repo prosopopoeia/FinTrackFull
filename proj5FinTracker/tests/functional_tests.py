91f5049770ebf4e795aedf3dfe1b2c1b7f80af12
Solomon Grundy <ngwtt@yahoo.com>
2021-12-21 07:49:20 -0700

    added login functional tests

--- proj5FinTracker/tests/functional_tests.py
+++ proj5FinTracker/tests/functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class VisitFinPageTest(unittest.TestCase):
@@ -11,12 +14,24 @@ class VisitFinPageTest(unittest.TestCase):
         
     def test_expected_Page_Retrieved(self):
         self.browser.get('http://127.0.0.1:8000/')
+        
+        #user visits page and sees login 
         self.assertIn('Fantastic Fin-Tracker', self.browser.title)
-        list_item1 = self.browser.find_element_by_tag_name('li').text
+        list_item1 = self.browser.find_element(By.TAG_NAME, 'li').text
         self.assertEqual(list_item1, "Month View")
-        #month_navigation_link = self.browser.find_element_by_id('navmonth')
-        #self.assertEqual( month_navigation_link.get_attribute('href'),'/vmonth') 
-        #self.fail('incorrect title, actual title is: ' + self.browser.title)
+        
+        # assumed register user logs into account
+        #usernamebox = self.browser.find_element_by_name('username')
+        usernamebox = self.browser.find_element(By.NAME, 'username')
+        passwordbox = self.browser.find_element(By.NAME, 'password')
+        usernamebox.send_keys('prosopopoeia')
+        passwordbox.send_keys('friendx')
+        loginbutton = self.browser.find_element(By.ID,'loginButton')
+        loginbutton.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
