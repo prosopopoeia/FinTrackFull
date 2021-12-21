5a38c0a297ee2f32ecafabb33fe08a5b66c9b0ac
Solomon Grundy <ngwtt@yahoo.com>
2021-12-20 21:02:38 -0700

    further functional tests

--- proj5FinTracker/tests/functional_tests.py
+++ proj5FinTracker/tests/functional_tests.py
@@ -12,7 +12,11 @@ class VisitFinPageTest(unittest.TestCase):
     def test_expected_Page_Retrieved(self):
         self.browser.get('http://127.0.0.1:8000/')
         self.assertIn('Fantastic Fin-Tracker', self.browser.title)
-        self.fail('incorrect title, actual title is: ' + self.browser.title)
+        list_item1 = self.browser.find_element_by_tag_name('li').text
+        self.assertEqual(list_item1, "Month View")
+        #month_navigation_link = self.browser.find_element_by_id('navmonth')
+        #self.assertEqual( month_navigation_link.get_attribute('href'),'/vmonth') 
+        #self.fail('incorrect title, actual title is: ' + self.browser.title)
 
 if __name__ == '__main__':
     unittest.main()
