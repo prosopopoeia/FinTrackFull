ef3cee294adbcb6249c8be1b2902758f6e125f71
Solomon Grundy <ngwtt@yahoo.com>
2021-12-23 20:28:54 -0700

    functional test work

--- proj5FinTracker/tests/functional_tests.py
+++ proj5FinTracker/tests/functional_tests.py
@@ -8,31 +8,97 @@ class VisitFinPageTest(unittest.TestCase):
     
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.get('http://127.0.0.1:8000/')
+        time.sleep(5)
+        try:    
+            self.login()
+            
+        except:
+            register_button = self.browser.find_element(By.ID, 'reg')
+            register_button.send_keys(Keys.ENTER)
+            #time.sleep(1)
+            
+            usernamebox = self.browser.find_element(By.NAME, 'username')
+            emailbox = self.browser.find_element(By.NAME, 'email')
+            passwordbox = self.browser.find_element(By.NAME, 'password')
+            confirmationbox = self.browser.find_element(By.NAME, 'confirmation')
+            submit = self.browser.find_element(By.NAME, 'submit-user')
+            
+            usernamebox.send_keys('testname')
+            emailbox.send_keys('email@email.com')
+            passwordbox.send_keys('testpassword')
+            confirmationbox.send_keys('testpassword')
+            submit.send_keys(Keys.ENTER)
+            
+            login_button = self.browser.find_element(By.ID, 'tlogin')
+            login_button.send_keys(Keys.ENTER)
+                   
+            self.login()
+            
+        #time.sleep(5)
+        
+        
+        
+    def login(self): 
+        usernamebox = self.browser.find_element(By.NAME, 'username')
+        passwordbox = self.browser.find_element(By.NAME, 'password')
+        usernamebox.send_keys('testname')
+        passwordbox.send_keys('testpassword')
+        submit_login = self.browser.find_element(By.ID,'submit-login')
+        submit_login.send_keys(Keys.ENTER)
+        #time.sleep(5)
+        
+        
 
     def tearDown(self):
         self.browser.quit()
         
-    def test_expected_Page_Retrieved(self):
-        self.browser.get('http://127.0.0.1:8000/')
+    def test_user_can_login(self):
+        #self.browser.get('http://127.0.0.1:8000/')
         
         #user visits page and sees login 
-        self.assertIn('Fantastic Fin-Tracker', self.browser.title)
+        self.assertIn('Fantastic Fin-Tracker', self.browser.title, "mismatch title indicates wrong page")
         list_item1 = self.browser.find_element(By.TAG_NAME, 'li').text
         self.assertEqual(list_item1, "Month View")
         
         # assumed register user logs into account
         #usernamebox = self.browser.find_element_by_name('username')
-        usernamebox = self.browser.find_element(By.NAME, 'username')
-        passwordbox = self.browser.find_element(By.NAME, 'password')
-        usernamebox.send_keys('prosopopoeia')
-        passwordbox.send_keys('friendx')
-        loginbutton = self.browser.find_element(By.ID,'loginButton')
-        loginbutton.send_keys(Keys.ENTER)
+        # usernamebox = self.browser.find_element(By.NAME, 'username')
+        # passwordbox = self.browser.find_element(By.NAME, 'password')
+        # usernamebox.send_keys('prosopopoeia')
+        # passwordbox.send_keys('friendx')
+        # loginbutton = self.browser.find_element(By.ID,'loginButton')
+        # loginbutton.send_keys(Keys.ENTER)
+        
+    def test_default_page_operations(self):
+        #self.browser.get('http://127.0.0.1:8000/')
+        time.sleep(1)
+        #user adds transaction
+        #!!!1 - hits 'Add transaction' to make input text boxes appear
+        add_trans_button = self.browser.find_element(By.ID, 'add-trans-button')
+        add_trans_button.send_keys(Keys.ENTER)
+        time.sleep(1)
+        add_transaction_amt = self.browser.find_element(By.ID, 'amt')
+        add_transaction_msg = self.browser.find_element(By.ID, 'msg')
+        add_transaction_cat = self.browser.find_element(By.ID, 'cat')
+        add_transaction_grp = self.browser.find_element(By.ID, 'grp')
+        add_transaction_button = self.browser.find_element(By.ID, 'add-btn')
+        
+        add_transaction_amt.send_keys("10")
+        add_transaction_msg.send_keys("10 test dollars")
+        add_transaction_cat.send_keys("test cat")
+        add_transaction_grp.send_keys("test group")
+        
+        add_transaction_button.send_keys(Keys.ENTER)
         time.sleep(1)
         
-        
-        
-
+        trans_table = self.browser.find_element(By.ID, 'target')
+        transactions = trans_table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('edit\ndelete\n2021-12-22\n10 test dollars\n10.00\ntest cat\ntest group', [transaction.text for transaction in transactions])
+        #for tranny in transactions:
+        #    print(f"number {nummy} is {tranny}")
+            
+            
 if __name__ == '__main__':
     unittest.main()
 #User logs into site
